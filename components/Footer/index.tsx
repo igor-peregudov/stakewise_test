72b4b14befb568e47cd640da92724d43f37892fd
iperegudov <igor.peregudov@thirdlane.com>
2022-08-04 23:37:07 +0100

    wip

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import HeartIcon from '../../public/heart.svg';
 import styles from './index.module.scss';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      Made with <img src={HeartIcon.src} alt="Love" />
+      Made with <Image src={HeartIcon.src} width="18px" height="16px" />
     </div>
   );
 };
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,18 +3,19 @@ import Link from 'next/link';
 import Logo from '../../public/logo.svg';
 import AccountIcon from '../../public/account.svg';
 import styles from './index.module.scss';
+import Image from 'next/image';
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <Link href={'/'}>
         <a>
-          <img src={Logo.src} alt="/" />
+          <Image src={Logo.src} width="28px" height="18px" />
         </a>
       </Link>
       <Link href={'#'}>
         <a>
-          <img src={AccountIcon.src} alt="/" />
+          <Image src={AccountIcon.src} width="18px" height="23px" />
         </a>
       </Link>
     </header>
